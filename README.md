c1d2ee123e518ceadbd7c699c2e1e42599a782e6
B12085 <bastian120.85@gmail.com>
2026-05-16 12:09:02 -0400

    arquetipo: estructura inicial y readme base

---
+++ README.md
@@ -0,0 +1,2 @@
+# SmartLogix - Sistema de Logística
+Este es el repositorio central para el backend de SmartLogix.
---
+++ src/test/java/ms_logistics_base/MsLogisticsBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ms_logistics_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLogisticsBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
